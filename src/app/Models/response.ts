bbabd2e1ecc35035673e73639ad5ca53a8e19755
javmarquez13 <javmarquez13@gmail.com>
2023-11-10 09:16:46 -0700

    Added folders structure

---
+++ src/app/Models/response.ts
@@ -0,0 +1,5 @@
+export interface Response{
+    success: string;
+    message: string;
+    data: any;
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './Components/home-page/home-page.component';
+import { LoginPageComponent } from './Components/login-page/login-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule,
